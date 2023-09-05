b1bb6b900c215ac49370e2229734666eddab2646
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-09-05 20:18:30 +0200

    register e login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <form action="#" method="post" class="form-control">
+        
+        <label for="email">la tua email</label> 
+        <br>
+        <input type="email" name="email" id="emailLogin">
+        <br>
+        <label for="password">la tua password</label> 
+        <input type="password" name="password" id="passwordLogin">
+        <br>
+        
+    
+    </form>
+</x-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <form action="#" method="post" class="form-control">
+        <label for="name">Il tuo nome</label> 
+        <br>
+        <input type="text" name="name" id="nameRegister">
+        <br>
+        <label for="email">la tua email</label> 
+        <br>
+        <input type="email" name="email" id="emailRegister">
+        <br>
+        <label for="password">la tua password</label> 
+        <br>
+        <input type="password" name="password" id="passwordRegister">
+        <br>
+        <label for="password_confirmation">conferma password</label>
+        <br> 
+        <input type="password" name="password_confirmartion" id="password_confirmartionRegister">
+    
+    </form>
+</x-layout>
