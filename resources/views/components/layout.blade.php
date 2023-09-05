f3363adaec7cef8c53c45f2d8aba71dce33ba943
Marcellocanduraa <marcellocandura.mc@gmail.com>
2023-09-05 20:48:12 +0200

    Livewire installation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,8 @@
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
+    <!-- livewire -->
+    @livewireStyles
 </head>
 <body>
    
@@ -23,6 +24,10 @@
     <header/>    
 
    {{ $slot }} 
+
+   <!-- livewire -->
+
+   @livewireStyles
    
 </body>
 </html>
