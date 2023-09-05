ad30a5a178591acd3120375495e8fc285873160a
jodydev <jodyossino2001@gmail.com>
2023-09-05 18:16:57 +0200

    fist-commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Titolo -->
+    <title>Presto.it</title>
+
+    <!-- Vite -->
+    <x-vite/>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+</head>
+<body>
+   
+    <!-- Navbar -->
+
+    <header>
+        <x-navbar/>
+    <header/>    
+
+   {{ $slot }} 
+   
+</body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+ <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+
+    <a class="navbar-brand ms-5 text-white" href="#">Presto.it</a>
+
+    <!-- collapse button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- search bar -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-nav me-auto ms-auto mb-2 mb-lg-0 py-1">
+          <input class="form-control me-2 my-searchbar" type="search" placeholder="Search" aria-label="Search">
+          <a href="#" class="my-btn-searchbar" type="submit"><i class="fa-solid fa-magnifying-glass fa-lg" style="color: #000000;"></i></a>
+      </div>
+    </div>
+
+    <!-- login | register -->
+    <form class="d-flex">
+        <a href="#" class="btn my-btn-login me-2" type="submit">Accedi</a>
+        <a href="#" class="btn my-btn-login me-2" type="submit">Registrati</a>
+    </form>
+
+  </div>
+</nav>
