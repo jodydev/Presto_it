f87da696e7e900b70115d03eebb03821e6d2b2d9
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-09-06 15:56:47 +0200

    migrations

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Announcement extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2023_09_06_131341_create_categories_table.php
+++ database/migrations/2023_09_06_131341_create_categories_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('announcements', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->float('price');
-            $table->longText('description');
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
-            
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('announcements');
+        Schema::dropIfExists('categories');
     }
 };
