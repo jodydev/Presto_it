c259341f36f3519c99151fce505916aabd9f3de1
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-09-06 21:06:30 +0200

    modified:   app/Http/Controllers/AnnouncementController.php

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    
     public function create(){
-        return view('announcements.create');
+        $announcements = Announcement::all();
+        $categories = Category::all();
+        return view('announcements.create', [
+            'announcements' => $announcement,
+            'categories' => $categories,
+        ]);
+    }
+
+    public function show($id){
+        $announcement = Announcement::find($id);
+
     }
 }
