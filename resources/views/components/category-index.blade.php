ba3943896aab608de06bfc975b223b2421d7438b
jodydev <jodyossino2001@gmail.com>
2023-09-08 21:22:38 +0200

    migration

---
+++ resources/views/components/category-index.blade.php
@@ -0,0 +1,19 @@
+<div class="container p-5 mt-4">
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                <div class="section-title">
+                    <h4>Tutte gli annunci</h4>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <ul class="filter__controls">
+                    <li class="active">Tutte le categorie</li>
+                    <li>Motori</li>
+                    <li>Abbigliamento</li>
+                    <li>Elettronica</li>
+                    <li>Casa</li>
+                    <li>Accessori</li>
+                </ul>
+            </div>
+        </div>
+</div>  
