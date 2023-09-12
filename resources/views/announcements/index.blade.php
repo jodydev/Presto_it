e6dc8d7f6287bd06f64a212fa733285bd4b35738
Alice83 <130662606+Alice83@users.noreply.github.com>
2023-09-12 22:25:18 +0200

    card tutti gli articoli

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -6,10 +6,10 @@
 
     </form>
 <section id="announcementCards">
-        <p class="h1 text-center">
+        <p class="h1 text-center mb-5">
             Tutti gli annunci
         </p>
-        @forelse ($announcements as $announcement)
+        <!-- OLD 12set2023 @forelse ($announcements as $announcement)
 
                         <div class="col-6">  
                             <div class="mb-3"><h1>{{$announcement->title}}
@@ -42,11 +42,47 @@
 
 
         @endforelse
+ -->
 
-        
+ <!-- CARD ELENCO ANNUNCI -->
+ <div class="container">
+        <div class="row">
+            @forelse ($announcements as $announcement)
+                <div class="col-lg-4 col-md-4 col-sm-6 px-5">
+                    <div class="trend__content">                  
+                        <div class="trend__item">
+                            <div class="trend__item__pic">
+                                <img src="https://picsum.photos/100/100" class="img-card-annuncement rounded-4" alt="">
+                            </div>
+                            <div class="trend__item__text">
+                                <h6>{{$announcement->title}}</h6>
+                                <strong>{{$announcement->category->title}}</strong>
+                                <div class="rating">
+                                    <i class="fa fa-star" style="color: #ff5114 !important"></i>
+                                    <i class="fa fa-star" style="color: #ff5114 !important"></i>
+                                    <i class="fa fa-star" style="color: #ff5114 !important"></i>
+                                    <i class="fa fa-star" style="color: #ff5114 !important"></i>
+                                    <i class="fa fa-star" style="color: #ff5114 !important"></i>
+                                </div>
+                                <div class="product__price">€{{$announcement->price}}</div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+                    
+                @empty    
 
+                <div class="col-12">
+                            <div class="alert">
+                                <p class="lead">
+                                    Non ci sono annunci per questa ricerca!!! ):
+                                </p>
+                            </div>
+                </div>
+        </div> 
+        </div>  
 
-
+    @endforelse
 
 </section>
 </x-layout>
