21429dc4df5b2f32751ef64c0ec0db05baf62b6d
Alice83 <130662606+Alice83@users.noreply.github.com>
2023-09-14 19:03:53 +0200

    show

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -60,7 +60,7 @@
         </div> -->
 
 <div id="details_announcement">
-   <div class="card">
+   <div class="card mb-5">
         <nav>
         <svg class="arrow" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><polygon points="352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 " stroke="#727272"/></svg>
         Torna agli annunci
@@ -68,7 +68,7 @@
         </nav>
   
         <div class="row">
-        <div class="col-sm-12 col-md-4">
+        <div class="col-sm-12 col-md-4 border-right pr-0">
             <div class="photo">
             <div id="carouselExample" class="carousel slide">
         <div class="carousel-inner">
@@ -102,15 +102,16 @@
             <p class= "card-text">{{$announcement->description}}</p>
             <button>Add to Cart</button>
             <button>Wishlist</button>
+
         </div>
-  
+
+
     </div>
     
     </div>
 
-
-  
 </div>
 
+<div></div>
 
 </x-layout>
