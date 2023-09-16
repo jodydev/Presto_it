25c4f126c832b3b499e991ba6c0c72fa414c7bd3
Alice83 <130662606+Alice83@users.noreply.github.com>
2023-09-16 16:46:39 +0200

    alert error

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,9 +1,11 @@
 @if ($errors->any())
-    <div class="alert alert-danger p-3 my-5">
+    <!-- <div class="alert alert-danger p-3 my-5"> -->
+    <div class="alert alert-danger alert-dismissible fade show" role="alert" style="margin-top:100px">  
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
