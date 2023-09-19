4b47ee21ef81378c0ae1289aa2e46b179cdf7424
jodydev <jodyossino2001@gmail.com>
2023-09-19 18:38:31 +0200

    hero fix

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -44,7 +44,7 @@
                         @if(!auth()->user())
 
                         <button class=" button-login" role="button"><a href="/login" style="color: #000000;"><i class="fa-solid fa-user" style="color: #000000;"></i> Accedi</a></button>
-                        <button class=" button-register" role="button" style="color: #ff5114;><a href="/register"><i class="fa-solid fa-right-to-bracket" style="color: #ff5114;"></i> Registrati</a></button>
+                        <button class=" button-register" role="button" ><a href="/register" style="color: #ff5114;"><i class="fa-solid fa-right-to-bracket" style="color: #ff5114;"></i> Registrati</a></button>
                        
                         @else
                         
