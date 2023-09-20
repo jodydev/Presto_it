20c740a37a7e37aefa4fe0c89ab4174c24228e37
Alice83 <130662606+Alice83@users.noreply.github.com>
2023-09-20 21:48:40 +0200

    alert

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -6,6 +6,7 @@
         <div style="font-size:20px">{{$message}}</div>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
+
 @endif
 
     <div class="container-fluid mt-5 ">
