aa826322a4cf6bc0ba83bb9f7660d9d528b66925
Alice83 <130662606+Alice83@users.noreply.github.com>
2023-09-20 22:06:41 +0200

    alert

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,7 +1,7 @@
 <section id="create" class="p-5 mt-5">
 
 @if (!is_null($message))
-    <div class="alert alert-success mt-5 fixed-top alert-dismissible fade show d-flex align-items-center" role="alert" style="margin-top:100px !important">
+    <div class="alert alert-success mt-5 fixed-top alert-dismissible fade show d-flex align-items-center" role="alert" style="margin-top:90px !important">
         <i class="fa-solid fa-circle-check me-3" style="color: #0f5132;"></i>    
         <div style="font-size:20px">{{$message}}</div>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
